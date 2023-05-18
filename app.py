ae9d839ed6edade232986d375db9e954dc7550c3
Yulmu <105157081+AileenWindzor@users.noreply.github.com>
2023-05-19 03:17:36 +0900

    Front by DH

---
+++ app.py
@@ -0,0 +1,28 @@
+# app.py
+from flask import Flask, request, jsonify, render_template
+import openai
+
+app = Flask(__name__)
+
+openai.api_key = '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'
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    data = request.get_json()
+    prompt = data['prompt']
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}],
+    )
+
+    return jsonify({
+        'response': response.choices[0]['message']['content']
+    })
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
